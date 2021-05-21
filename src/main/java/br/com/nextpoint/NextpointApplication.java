3676b8b7ec5b8846d7200384ad90dfbef41d6c87
Allan Patrick <Allan.15.patrick@gmail.com>
2021-05-20 22:47:50 -0300

    Adicionando JPA

--- src/main/java/br/com/nextpoint/NextpointApplication.java
+++ src/main/java/br/com/nextpoint/NextpointApplication.java
@@ -1,13 +1,22 @@
 package br.com.nextpoint;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class NextpointApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NextpointApplication.class, args);
+
+		EntityManagerFactory EMF = Persistence.createEntityManagerFactory("contas");
+		EntityManager EM = EMF.createEntityManager();
+		EMF.close();
 	}
 
 }
