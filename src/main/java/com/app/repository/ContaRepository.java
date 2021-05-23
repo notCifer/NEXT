4719df68332aea6071573a6916ac9ae030fb9b39
Allan Patrick <Allan.15.patrick@gmail.com>
2021-05-23 20:14:19 -0300

    Resolvendo Bug de conexão

--- src/main/java/com/app/repository/ContaRepository.java
+++ src/main/java/com/app/repository/ContaRepository.java
@@ -1,13 +1,8 @@
 package com.app.repository;
 
-import java.util.List;
-
 import com.app.model.Conta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContaRepository extends JpaRepository<Conta, String> {
-
-    List<Conta> findByNome(String nome);
-
 }
